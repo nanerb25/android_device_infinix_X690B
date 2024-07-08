ebeed74f43c63c544dba9da01b5907d3c5a58f59
eun0115 <gianpaoloestacio5@gmail.com>
2024-07-08 00:48:44 +0000

    X657C: Build COnfigstore
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     GoogleCameraGo
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service \
+    android.hardware.configstore@1.1-service
+
 # DT2W
 PRODUCT_PACKAGES += \
     DT2W-Service-X657C
