114bf55f5959c4528391ba3cc7510baacc93b0cd
Mustafa <ravadies@protonmail.com>
2024-09-14 13:47:06 +0300

    aosp_base disable function (see TODO)

--- lineage_X690B.mk
+++ lineage_X690B.mk
@@ -24,7 +24,9 @@ $(call inherit-product, device/infinix/X690B/device.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# TODO:why we need a aosp_base.mk ?
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
